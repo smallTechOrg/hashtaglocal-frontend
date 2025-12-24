678a2a86cf41ff0f6e807d04b7b7b05f3853962b
prachinayak1509 <prachinayak1509@gmail.com>
2025-12-24 12:43:38 +0530

    ui changes

--- app/issueDetail.tsx
+++ app/issueDetail.tsx
@@ -1,20 +1,23 @@
-import IssueCard from "@/components/IssueCard";
+
 import { MaterialIcons } from "@expo/vector-icons";
 import React from 'react';
-import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import '../global.css';
+import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
 import CustomText from "@/components/CustomText";
+import IssueImage from "@/components/IssueImage";
+import { Linking } from "react-native";
 
 const IssueDetailScreen = () => {
     return (
         <ScrollView className="px-2" >
             {/* Main Image Header */}
-            <IssueCard
+            <IssueImage
                 imageSource={require("../assets/plothole.jpg")}
                 location="Iblur Lake Rd. Lat: 27.1751°N Long: 78.0421°E"
                 timestamp="2025-12-17 15:30:44 IST"
                 daysActive="17days"
-                onShare={() => console.log('Shared!')}
+                onShare={() => {
+                    Linking.openURL("https://smalltech.in");
+                }}
                 className="self-center mt-4"
             />
 
@@ -30,29 +33,33 @@ const IssueDetailScreen = () => {
                 </View>
             </View>
 
-            {/* Map and Description Section */}
-            <View className="flex-row py-2">
-                <View className="w-1/2 pr-2">
+            {/* Map + Description */}
+            <View className="flex-row py-4">
+                <View className="w-1/2 pr-2 items-center">
+                    {/* Map */}
                     <View className="h-32 w-full">
                         <Image
                             source={require("../assets/map.png")}
-                            className="h-full w-full"
+                            className="h-full w-full rounded-lg"
                         />
                     </View>
-                </View>
-                <View className="w-1/2 pl-2">
-                    <CustomText className="text-gray-700 text-sm leading-5 text-center">
-                        From the looks of it, it looks like a beautiful aesthetic crater. Like it was destined to be part of this road...
+                    {/* Verified */}
+                    <CustomText className="mt-3 text-sm text-gray-700">
+                        Verified by{" "}
+                        <CustomText className="font-bold text-black text-lg">16</CustomText>{" "}
+                        locals
                     </CustomText>
                 </View>
-            </View>
-
-            {/* Verification and Action */}
-            <View className="flex-row justify-between items-center py-4">
-                <CustomText>Verified by <CustomText className="font-bold text-black text-lg">16</CustomText> locals</CustomText>
-                <TouchableOpacity className="bg-[#82C458] px-6 py-3 rounded-xl active:opacity-80">
-                    <CustomText className="text-white">Update Issue</CustomText>
-                </TouchableOpacity>
+                <View className="w-1/2 pl-2 items-center">
+                    {/* Description */}
+                    <CustomText className="text-gray-700 text-sm leading-5 text-center">
+                        From the looks of it, it looks like a beautiful aesthetic crater. Like it
+                        was destined to be part of this road...
+                    </CustomText>
+                    <TouchableOpacity className="mt-4 bg-[#82C458] px-2 py-2 rounded-xl active:opacity-80">
+                        <CustomText className="text-white">Update Issue</CustomText>
+                    </TouchableOpacity>
+                </View>
             </View>
         </ScrollView>
     );
