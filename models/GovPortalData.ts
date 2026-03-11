38e482ce887b69140f93c716f149283018697636
Vivek Agarwal <viveksinghalp@gmail.com>
2026-03-11 15:05:46 +0530

    issue model updated

---
+++ models/GovPortalData.ts
@@ -0,0 +1,8 @@
+export interface GovPortalData {
+  tracking_id: string;
+  status: string;
+  portal_name: string;
+  portal_track_link: string;
+  meta_data?: Record<string, any>;
+  updated_at?: string;
+}
