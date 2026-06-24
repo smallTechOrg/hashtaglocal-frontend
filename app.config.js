3833d77f1a5fe2e338cc2df55c35e366bdda89d4
prachinayak1509 <prachinayak1509@gmail.com>
2026-06-24 14:50:53 +0530

    Google Services files from tracking, use EAS Secrets

--- app.config.js
+++ app.config.js
@@ -34,7 +34,7 @@ export default {
     name: "#local",
     slug: "hashtaglocal",
     owner: "smalltech",
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: "portrait",
     icon: "./assets/app-icon.png",
     scheme: "hashtaglocal",
