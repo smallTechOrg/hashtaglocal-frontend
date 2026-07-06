5d9de51d50a4bab1a45951b632d6070f518b514a
prachinayak1509 <prachinayak1509@gmail.com>
2026-07-06 20:49:54 +0530

    build update

--- app.config.js
+++ app.config.js
@@ -34,7 +34,7 @@ export default {
     name: "#local",
     slug: "hashtaglocal",
     owner: "smalltech",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/app-icon.png",
     scheme: "hashtaglocal",
